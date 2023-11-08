d5165b2a2feb366ac8013b23e33bccfea0a8d0b8
Nam3001 <nguyenvn30012003@gmail.com>
2023-11-08 07:48:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('<h1>Something went wrong!</h1>')
+})
+
+app.get('/redirect', (req, res) => {
+    res.redirect(301, 'https://javhay.movie/em-thu-ky-rieng-dam-dang-va-ten-sep-doi-bai-cuong-dam/')
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+})
